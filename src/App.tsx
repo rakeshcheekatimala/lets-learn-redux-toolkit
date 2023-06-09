0b4990f701df7c9ac7ec8fa69d16445bac7a23ff
rakeshcheekatimala <rakeshcheekatimala@RAKESHs-MacBook-Pro.local>
2023-06-09 23:56:05 +0800

    feat: custom hooks for store
    - example to write reducers with payload & without.
    - dispatch actions & test the redux flow.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useAppDispatch, useAppSelector } from "./hooks";
+import reactLogo from "./assets/react.svg";
+import {
+  increment,
+  incrementWithValue,
+} from "./features/counter/counter-slice";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
 
+  function onClickHandler() {
+    dispatch(increment());
+  }
   return (
     <>
       <div>
@@ -16,20 +24,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React with Redux Toolkit </h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={onClickHandler}>Increment</button> &nbsp;
+        <button onClick={() => dispatch(incrementWithValue(5))}>
+          Increment by 5
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Value of count is {count}</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/features/counter/counter-slice.ts
+++ src/features/counter/counter-slice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface CounterState {
   value: number;
@@ -18,9 +18,13 @@ const counterSlice = createSlice({
     decrement(state: CounterState) {
       state.value--;
     },
+    incrementWithValue(state: CounterState, action: PayloadAction<number>) {
+      state.value += action.payload;
+    },
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const { increment, decrement, incrementWithValue } =
+  counterSlice.actions;
 
 export default counterSlice.reducer;
